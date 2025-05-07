f2a4a24d55f23e99ec9db0aa972e8e16b3818f34
esamper <chaconyadixa123@gmail.com>
2025-05-07 16:12:53 -0400

    nuevos requerimientos

--- app/extension.py
+++ app/extension.py
@@ -1,9 +1,12 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db=SQLAlchemy()
 migrate=Migrate()
+login=LoginManager()
 
 def iniciar_extensiones(app):
     db.init_app(app)
+    login.init_app(app)
     migrate.init_app(app, db)
---
+++ app/forms/auth/atuenticacion.py
@@ -0,0 +1,17 @@
+# app/auth/forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class LoginForm(FlaskForm):
+    usuario = StringField(
+        'Usuario',
+        validators=[DataRequired(message="El usuario es obligatorio"),
+                    Length(min=3, max=100)]
+    )
+    clave = PasswordField(
+        'Contraseña',
+        validators=[DataRequired(message="La contraseña es obligatoria")]
+    )
+    recordar = BooleanField('Recordarme')
+    submit = SubmitField('Iniciar Sesión')
